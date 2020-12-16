ebb94f6d78ff80f079eea939a7f85287f85206c5
IzMichael <themichael77@outlook.com>
2020-12-16 19:22:13 +1300

    Build Bot

---
+++ README.md
@@ -0,0 +1,17 @@
+# AmongIz
+
+## Installation
+You will need to follow [this guide](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot) to set up the bot account.
+1. Run `npm i AmongIz` in the command line
+2. Create a `config.json` file in the directory that contains `main.js`
+3. Paste the following into `config.json`
+```
+{
+    "token": "BOT TOKEN"
+}
+```
+4. Replace `BOT TOKEN` with your Bot Token
+5. Save the file
+6. Run `node .`
+7. Now, follow [this guide](https://discordjs.guide/preparations/adding-your-bot-to-servers.html#bot-invite-links) to add the bot to your server
+8. Once you've added the bot to your server, run `iz!help` to get a list of commands
---
+++ main.js
@@ -0,0 +1,73 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+const config = require('./config.json');
+
+const prefix = 'iz!';
+
+var playerlist = [];
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    remaining = (10 - playerlist.length)
+    const playerlistmsg = "**Current Players:**\n" + playerlist.join('\r\n') + "\n\n**There are " + playerlist.length + " players**\n" + remaining + " players are needed for a full game";
+
+    // Commands \/
+
+    if (command === 'add') {
+        const taggedUser = message.mentions.users.first();
+        console.log(taggedUser);
+        playerlist.push(taggedUser);
+        message.channel.send(playerlistmsg);
+        console.log(playerlist);
+        return
+    } else if (command === 'clear') {
+        playerlist = [];
+        message.channel.send("Cleared the Player List");
+        return
+    } else if (command === 'list') {
+        if (!playerlist.length) {
+            return message.channel.send("The Player List is currently empty. Use `iz!add @USER` to add a player, or `iz!help` for more commands.")
+        }
+        message.channel.send(playerlistmsg);
+        return
+    } else if (command === 'help') {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('AmongIz Commands List')
+            .addFields({
+                name: '`iz!add @USER`',
+                value: 'Adds a player to the list of all current players',
+                inline: true
+            }, {
+                name: '`iz!clear`',
+                value: 'Clears the list of all current players',
+                inline: true
+            }, {
+                name: '`iz!list`',
+                value: 'Lists all current players',
+                inline: true
+            }, )
+
+        message.channel.send(helpEmbed);
+        return
+    } else {
+        message.channel.send("Command not Found")
+        return
+    }
+});
+
+// login to Discord with your app's token
+client.login(config.token);
